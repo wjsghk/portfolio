d299ec8c101986fb2cb4a18a70cfa3dc903ffa28
wjsghk <kkwe3208@naver.com>
2023-03-13 15:42:09 +0900

    +css

---
+++ main.js
@@ -0,0 +1,5 @@
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
